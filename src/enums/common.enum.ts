f9e2fed391cc782e9f79193fdb6f45d0c3b94ced
Thanakorn Nitsiri <tnitsiri@gmail.com>
2025-05-07 16:11:02 +0700

    feat: [1.0.4] common enum

---
+++ src/enums/common.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * ANCHOR Env Name Enum
+ * @date 07/05/2025 - 16:10:05
+ *
+ * @export
+ * @enum {number}
+ */
+export enum EnvNameEnum {
+  Development = 'development',
+  Production = 'production',
+}
