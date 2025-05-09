bf35a7114a0c7c3c15ee9d31c8117bdbfebbb20c
Thanakorn Nitsiri <tnitsiri@gmail.com>
2025-05-09 11:24:12 +0700

    feat: [1.0.16] add user member model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,3 +1,5 @@
+import { MemberModel } from './member.model';
+
 /**
  * ANCHOR User Model
  * @date 08/05/2025 - 03:26:51
@@ -10,4 +12,5 @@ export interface UserModel {
   id: string;
   name: string;
   email: string;
+  members: MemberModel[];
 }
