52a8749c96ce3f3ea09f8dedc085a01aa3987cbe
Thanakorn Nitsiri <tnitsiri@gmail.com>
2025-05-09 12:18:57 +0700

    feat: [1.0.17] form enum

---
+++ src/enums/form.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * ANCHOR Form Mode Enum
+ * @date 09/05/2025 - 12:18:16
+ *
+ * @export
+ * @enum {number}
+ */
+export enum FormModeEnum {
+  Create = 'CREATE',
+  Update = 'UPDATE',
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './common.enum';
+export * from './form.enum';
 
 export * from './member.enum';
